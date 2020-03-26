7ab807bc4a874466c8612a78a0d2985ac684962f
mathiascordova <macordova@miuandes.cl>
2020-03-26 11:04:12 -0300

    Incremento 1 completo

--- laboratorio-02-mathiascordova/laboratorio-02-mathiascordova/Program.cs
+++ laboratorio-02-mathiascordova/laboratorio-02-mathiascordova/Program.cs
@@ -10,6 +10,33 @@ namespace laboratorio_02_mathiascordova
     {
         static void Main(string[] args)
         {
+            string seleccion;
+            Espotifai spotify = new Espotifai();
+            Console.WriteLine("Bienvenido a Espotifai!");
+
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Indicar el numero al que se quiere acceder:");
+                Console.WriteLine("1. Ver canciones");
+                Console.WriteLine("2. Agregar una cancion");
+                Console.WriteLine("3. Salir de Espotifai");
+                seleccion = Console.ReadLine();
+                if (seleccion == "1")
+                {
+                    spotify.VerCanciones();
+                }
+                if (seleccion == "2")
+                {
+                    Console.WriteLine("Escriba nombre, album, artista y genero (en ese orden)");
+                    Console.WriteLine(spotify.AgregarCancion(new Cancion(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine())));
+                }
+                else if (seleccion == "3")
+                {
+                    Console.WriteLine("Hasta Pronto!");
+                    break;
+                }
+            }
 
         }
     }
